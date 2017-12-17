4a4a1edd29aca9404bc614f197fb0bc960e9ed4a
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-12-17 13:15:17 +0100

    add properties of phone

--- scripts.js
+++ scripts.js
@@ -3,3 +3,7 @@ function Phone(brand, price, color) {
 	this.price = price;
 	this.color = color;
 }
+
+Phone.prototype.printInfo = function(){
+	console.log("This phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + " . ");
+}
