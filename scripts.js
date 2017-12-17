0a0f2b8347c11954dc3c7ea895fd6e07e3372594
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-12-17 13:25:38 +0100

    add 3 new objects

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, screen) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
+	this.screen = screen;
 }
 
 Phone.prototype.printInfo = function(){
-	console.log("This phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + " . ");
+	console.log("This phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + " . Also, this phone got the beautiful " + this.screen + " screen. ");
 }
+
+var iPhone6S = new Phone("Apple", 2250, "silver", "3 inch");
+iPhone6S.printInfo();
+
+var SamsungS6 = new Phone("Samsung", 1850, "black", "2,5 inch");
+SamsungS6.printInfo();
+
+var OnePlusOne = new Phone("OnePlus", 550, "gold", "1 inch");
+OnePlusOne.printInfo();
